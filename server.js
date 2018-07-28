431dd22b18f7f49ca17c55541729d050d799384a
Prakash Hansaria <prakash.hansaria@gmail.com>
2018-07-28 12:05:53 +0530

    Final Commit

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
-const express = require('express')
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
 const app = express()
 
-app.set('view engine', 'ejs');
+const apiKey = '99553ac69222457327578afb69fcacf9';
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
-app.get('/', function(req,res){
-  res.render("index")
-});
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
 
-app.listen(3000, function(){
-  console.log("Listening to port 3000");
-});
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!')
+})
